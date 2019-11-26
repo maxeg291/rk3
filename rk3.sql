cb2814ea1426fe0af0b921d9287b045cee4b85ea
maxeg291 <56290453+maxeg291@users.noreply.github.com>
2019-11-26 09:44:30 +0300

    Update rk3.sql

--- rk3.sql
+++ rk3.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXIST `goods` (
+CREATE TABLE IF NOT EXISTS `goods` (
  `name` varchar(20) NOT NULL,
  `manufacturer` varchar(20) NOT NULL,
  `description` varchar(120) NOT NULL,
@@ -11,8 +11,8 @@ INSERT INTO `goods` (`name`,`manufacturer`,`description`,`count`) VALUES
  ('socks','factory','clothes','0'),
  ('drill','plant','instrument','800'),
  ('salt','factory','food','1100'),
- ('ksugar','factory','food',600'),
+ ('ksugar','factory','food','600'),
  ('kpepper','factory','food','1200'),
  ('tomato','garden','food','0');
 DELETE FROM `goods` WHERE `count` = 0;
-SELECT * FROM `goods` WHERE `name` LIKE (k%) AND `count`<1000 AND `count`>300;
+SELECT * FROM `goods` WHERE `name` LIKE ('k%') AND `count`<1000 AND `count`>300;
